cbfbf70330e3ed53eae73085e4e0784d79d9697b
SoraPay <157640120+SoraPay@users.noreply.github.com>
2024-06-13 23:45:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,69 +1,44 @@
-﻿using System.Diagnostics.Metrics;
-using System.Security.Principal;
-using System.Threading.Channels;
-
 namespace S1
 {
     class s1
     {
         static void Main(string[] args)
         {
-
-            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
-            int[] digits = new int[a.Length];
-
-            AddInArrayDigits(a, digits);
-            PrintArray(digits);
-
-            Array.Sort(digits);
-
-            Console.WriteLine();
-
-            PrintArray(digits);
-
-            Console.WriteLine();
-
-            RebuildArray(a,digits);
-
-            Console.WriteLine();
-
-            PrintArray(a);
-
-            Console.WriteLine();
-
-        }
-
-        static void AddInArrayDigits(int[,] allArray, int[] arrayForAdd)
-        {
-            int account = 0;
-            for (int i = 0; i < allArray.GetLength(0); i++)
-                for (int j = 0; j < allArray.GetLength(1); j++)
+            if (args.Length == 3)
+            {
+                if (float.TryParse(args[0], out float value1) && float.TryParse(args[2], out float value2))
                 {
-                    arrayForAdd[account] = allArray[i, j];
-                    account++;
-                }
-        }
+                    switch (args[1])
+                    {
+                        case "*":
+                            Console.WriteLine(value1 * value2);
+                            break;
 
-        static void PrintArray(int[] array) => Console.Write(string.Join(" ", array));
+                        case "/":
+                            if (value2 != 0)
+                            {
+                                Console.WriteLine(value1 / value2);
+                            }
+                            else Console.WriteLine("я не могу поделить на 0");
+                            break;
 
-        static void PrintArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i,j] + " ");
-                }
-        }
+                        case "+":
+                            Console.WriteLine(value1 + value2);
+                            break;
 
-        static void RebuildArray(int[,] rebuildArray, int[] additionArray)
-        {
-            int account = 0;
-            for (int i = 0; i < rebuildArray.GetLength(0); i++)
-                for (int j = 0; j < rebuildArray.GetLength(1); j++)
-                {
-                    rebuildArray[i, j] = additionArray[account];
-                    account++;
+                        case "-":
+                            Console.WriteLine(value1 - value2);
+                            break;
+
+                        default:
+                            Console.WriteLine("Используй только + - / *");
+                            break;
+
+                    }
                 }
+                else Console.WriteLine("Первое и второе значение должны быть числами");
+            }
+            else Console.WriteLine("Формат ввода a + b, a - b, a / b, a * b");
         }
     }
 }
